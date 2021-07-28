e2e331a93bbc2cb3a984d3f7e0a3ac9b3f192642
Toktam <ttmohebbi@gmail.com>
2021-07-27 20:01:48 -0400

    finish building the getUser method

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ const app = Vue.createApp({
       const res = await fetch('https://randomuser.me/api')
       const { results } = await res.json()
 
-      console.log(results)
+      // console.log(results)
 
-      this.firstName = 'John'
-      this.lastName = 'Doe'
-      this.email = 'john@doe.com'
-      this.gender = 'male'
-      this.picture = 'https://randomuser.me/api/portraits/men/10.jpg'
+      this.firstName = results[0].name.first
+      this.lastName = results[0].name.last
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = results[0].picture.large
     }
   }
 })
